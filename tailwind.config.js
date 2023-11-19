52363cdbd6e851aa94b54880318c65611c83f86b
thuyencode <thuyencode@proton.me>
2023-11-19 21:24:56 +0700

    Add new font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        logo: ['Norse-Bold', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
