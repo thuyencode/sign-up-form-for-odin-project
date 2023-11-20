084ec082bd37b072deedf2fd9e80cf1179b8ce9b
thuyencode <thuyencode@proton.me>
2023-11-20 11:45:00 +0700

    Fix a bug caused by vite-plugin-html by removing it

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,5 @@
 import { defineConfig } from 'vite'
-import { createHtmlPlugin } from 'vite-plugin-html'
 
 export default defineConfig({
   base: '/sign-up-form-for-odin-project/',
-  plugins: [
-    createHtmlPlugin({
-      minify: true,
-    }),
-  ],
 })
